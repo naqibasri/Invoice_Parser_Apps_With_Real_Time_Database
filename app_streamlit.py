2a8ebfa71c87ad73e84295a22c0b6a9a8e67205b
naqibasri <naqib.asri.294@gmail.com>
2024-03-21 16:35:38 +0800

    dataframe added

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,49 +3,23 @@
 
 from logging import info
 from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  # take environment variables from .env
 
 import streamlit as st
 import os
 import pathlib
 import textwrap
 from PIL import Image
-
-
+import pandas as pd
 import google.generativeai as genai
+import ast
 
+from utils import *
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
-
-def get_gemini_response(input,image,prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input,image[0],prompt])
-    return response.text
-    
-
-def input_image_setup(uploaded_file):
-    # Check if a file has been uploaded
-    if uploaded_file is not None:
-        # Read the file into bytes
-        bytes_data = uploaded_file.getvalue()
-
-        image_parts = [
-            {
-                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
-                "data": bytes_data
-            }
-        ]
-        return image_parts
-    else:
-        raise FileNotFoundError("No file uploaded")
-
-
 ##initialize our streamlit app
-
 st.set_page_config(page_title="INVOICE PARSER APPS")
 def parser():
     st.header("INVOICE PARSER APPS")
@@ -64,12 +38,12 @@ def parser():
                    You will receive input images as invoices &
                    you need to provide the value based on the input invoice. 
                    """
-
     ## If ask button is clicked
     question = {'InvoiceNum':"What is the invoice number? please just give me the value only",
                 'InvoiceDate':"What is the invoice date? please just give me the value only",
-                'InvoiceItem':'Can you provide a list of dicts of the item name, quantity and price of each of the items listed in the invoice. Please give in the format[{"name": "Rubber","quantity": 1,"price": 270.0},...]',
+                'InvoiceItem':'Please provide a dict of list of the items name, quantity and price of each of the items listed in the invoice. Please give in the format {"name": ["Rubber","Pencil","Book",...],"quantity": [1,3,5,...],"price": [270.0,130.0,80.0,...]}. Provide the dict only without any futher text',
                 'InvoiceDiscount':"Can you provide the amount of discount in the invoice if any. If not available, please return 0",
+                'InvoiceTaxes': "Can you provide the amount of taxes in the invoice if any. If not available, please return 0",
                 'InvoiceTotal':"What is the final amount of the invoice after discount and taxes? please just give me the value only"}
     if submit:
         responses=[]
@@ -78,12 +52,20 @@ def parser():
             response=get_gemini_response(input_prompt,image_data,question[info])
             responses.append(response)
             #st.subheader("The Response is")
-        for response in responses:
-            st.write(response) 
+
+        df_invoice = pd.DataFrame({'InvoiceNum':[responses[0]],
+                           'InvoiceDate':[responses[1]],
+                           'InvoiceTotal':[responses[5]]})
+        st.dataframe(df_invoice)
+        items = ast.literal_eval(responses[2])
+        df_item = pd.DataFrame(items)
+        st.dataframe(df_item)
+ 
 
 def database():
     st.header("CHECK YOUR CURRENT DATABASE HERE")
-    
+    #if database:
+        #st.dataframe(database)
 def analysis():
     st.header("CHECK YOUR ANALYSIS HERE")
 
